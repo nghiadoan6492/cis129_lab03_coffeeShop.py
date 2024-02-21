76e51c6fdc80311e7c948930f6b23d23cb4f5ad4
nghiadoan6492 <159955769+nghiadoan6492@users.noreply.github.com>
2024-02-20 23:02:57 -0700

    Create cis129_lab03_coffeeShop.py

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,68 @@
+#Coffee shop receipt
+"""Find the total price of items after tax."""
+
+#Declare and assign variable tax
+tax=0.06 
+
+#Ask the user for the price of coffees and muffins
+coffee=input("what is the price of a coffee?"))
+what is the price of a coffee?5
+
+muffin=input("what is the price of a muffin?"))
+what is the price of a muffin?4 
+
+#Calculate and display the subtotal amount before tax
+subtotal=coffee + muffin
+
+print("the subtotal is")
+the subtotal is
+print(subtotal)
+9
+
+#Calculate and display the tax amount
+taxamount=subtotal*tax
+
+print("sale tax is")
+sale tax is
+print(taxamount)
+0.54
+
+#Calculate and display the total amount
+totalamount=subtotal + taxamount
+print("your totalamount is")
+your totalamount is
+print(totalamount)
+9.54
+
+print("""**********************************
+    ...: My Coffee and Muffin Shop
+    ...: Number of coffees bought?
+    ...: 1
+    ...: Number of muffins bought?
+    ...: 2
+    ...: **********************************
+    ...: 
+    ...: **********************************
+    ...: My Coffee and Muffin Shop Receipt
+    ...: 1 Coffee at $5 each: $ 5.00
+    ...: 2 Mufffins at $4 each: $ 8.00
+    ...: 6% tax: $.78
+    ...: -------
+    ...: Total: $ 13.78
+    ...: **********************************""")
+**********************************
+My Coffee and Muffin Shop
+Number of coffees bought?
+1
+Number of muffins bought?
+2
+**********************************
+
+**********************************
+My Coffee and Muffin Shop Receipt
+1 Coffee at $5 each: $ 5.00
+2 Mufffins at $4 each: $ 8.00
+6% tax: $.78
+-------
+Total: $ 13.78
+**********************************
